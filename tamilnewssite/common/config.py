8ca699546aa32b9229d35a7666fe900620668e30
Sriram <43099277+ColumbusCoders@users.noreply.github.com>
2018-09-14 00:19:35 -0400

    updated config file

--- tamilnewssite/common/config.py
+++ tamilnewssite/common/config.py
@@ -1,4 +1,4 @@
 cinema_urls = ["http://cinema.dinakaran.com/Rss/RssKollywood.aspx","http://feeds.feedburner.com/Puthiyathalaimurai_Cinema_News"]
 main_urls = ["http://www.dinakaran.com/rss_Latest.asp"]
-sport_urls = ["http://www.dinakaran.com/rss_news.asp?id=16"]
+sport_urls = ["http://www.bbc.co.uk/tamil/sport/index.xml"]
 business_urls = ["http://rss.dinamalar.com/?cat=business1","http://www.dinakaran.com/rss_news.asp?id=15"]
